8a96343a2268c91fb0fa74d5fdd464afef49dc82
mohan <mohancodes5.gmail.com>
2026-05-09 16:41:39 +0530

    image issue fixed

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     # Old /admin/ bookmarks -> custom admin (Django admin UI is disabled).
@@ -14,3 +15,14 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Production (e.g. Render): django.conf.urls.static.static() only wires media when DEBUG=True,
+    # so we serve uploads explicitly. For durability across deploys, use a persistent disk or S3.
+    media_url = settings.MEDIA_URL.lstrip("/")
+    urlpatterns += [
+        re_path(
+            rf"^{media_url}(?P<path>.*)$",
+            serve,
+            {"document_root": settings.MEDIA_ROOT},
+        ),
+    ]
